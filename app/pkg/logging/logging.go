fed356b9353d35b27acfac591f7e16334448b540
gerchek <gerchekgerchek1@gmail.com>
2022-08-02 15:41:02 +0500

    full logrus

--- app/pkg/logging/logging.go
+++ app/pkg/logging/logging.go
@@ -3,6 +3,7 @@ package logging
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"runtime"
 
@@ -11,7 +12,29 @@ import (
 
 const logFile = "logs/project.log"
 
+type writerHook struct {
+	Writer    []io.Writer
+	LogLevels []logrus.Level
+}
+
 var log *logrus.Logger
+
+func (hook *writerHook) Fire(entry *logrus.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
+	}
+
+	for _, w := range hook.Writer {
+		w.Write([]byte(line))
+	}
+
+	return err
+}
+
+func (hook *writerHook) Levels() []logrus.Level {
+	return hook.LogLevels
+}
 
 func init() {
 	log = logrus.New()
@@ -25,14 +48,25 @@ func init() {
 		},
 	})
 
+	log.Out = ioutil.Discard
+
 	f, err := os.OpenFile("logs/project.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
 
-	wrt := io.MultiWriter(os.Stdout, f)
+	// wrt := io.MultiWriter(os.Stdout, f)
 
-	log.SetOutput(wrt)
+	// log.SetOutput(wrt)
+
+	// log.SetOutput(io.MultiWriter(f, os.Stdout))
+
+	log.SetOutput(io.Discard)
+
+	log.AddHook(&writerHook{
+		Writer:    []io.Writer{f},
+		LogLevels: logrus.AllLevels,
+	})
 
 }
 
